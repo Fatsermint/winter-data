d7f4310e93b89b59f11b88a76f4a97aae367597d
fatsermint <kyosti.ahti@gmail.com>
2025-11-29 13:03:03 +0200

    Added more date to select

--- script.js
+++ script.js
@@ -1,69 +1,86 @@
 let currentDate = new Date().getTime()
 
 
-let url =`https://geoserver2.ymparisto.fi/geoserver/eo/wms?time=${currentDate}`
+document.querySelectorAll(".changes-map-mode").forEach(el => {
+  el.addEventListener("click", () => {
+    reloadData(el.getAttribute("data-mode"))
+  })
+})
+
+const layerTypes = {
+  lake_ice: 'eo:EO_MR_VIIRS_LIE',
+  snow: 'eo:EO_FSC',
+  rannikko_jarvi: 'eo:EO_SST'
+}
+let selected = "lake_ice"
+
+let url = `https://geoserver2.ymparisto.fi/geoserver/eo/wms?time=${currentDate}`
 const reloadAutoCheckBox = document.querySelector("#reloadAuto")
 
-var map = L.map('map').setView([62.192059, 24.945831], 5);
+let map = L.map('map').setView([62.192059, 24.945831], 5);
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-}).addTo(map);
-;
-var wmsLayer = L.tileLayer.wms(url, {
-    layers: 'eo:EO_MR_VIIRS_LIE'
-}).addTo(map);
-function reloadData() {
-  wmsLayer.removeFrom(map)
+  attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+}).addTo(map)
+  
+
+
+const dateSelector = document.querySelector("#date")
+
+dateSelector.addEventListener("input", (event) => {
+  console.log(dateSelector.value)
+  currentDate = new Date(dateSelector.value).getTime()
+  url = `https://geoserver2.ymparisto.fi/geoserver/eo/wms?time=${new Date(currentDate).toISOString().substring(0, 10)}`
+  if (reloadAutoCheckBox.checked) {
+    reloadData()
+  };
+});
+
+let wmsLayer
+function reloadData(layerType) {
+  if (wmsLayer) wmsLayer.removeFrom(map)
   wmsLayer = L.tileLayer.wms(url, {
-    layers: 'eo:EO_MR_VIIRS_LIE',
-    opacity: input.value * 0.01,
-}).addTo(map);
+    layers: layerTypes[layerType || selected]
+  }).addTo(map)
+  if (layerType) selected = layerType
+  console.log(selected)
 }
+reloadData()
 function addDays(amount) {
   currentDate += 1000 * 3600 * 24 * amount;
-  url =`https://geoserver2.ymparisto.fi/geoserver/eo/wms?time=${new Date(currentDate).toISOString().substring(0, 10)}`;
-  console.log(new Date(currentDate));
-  if (reloadAutoCheckBox.checked)  {
+  dateSelector.value = new Date(currentDate).toISOString().substring(0, 16)
+
+  console.log(dateSelector.value)
+  url = `https://geoserver2.ymparisto.fi/geoserver/eo/wms?time=${new Date(currentDate).toISOString().substring(0, 10)}`
+  console.log(new Date(currentDate).toISOString().substring(0, 16))
+  if (reloadAutoCheckBox.checked) {
     reloadData()
-    console.log("ADSSSSSSSSSSSSSS")
-  }
+  };
 }
 const value = document.querySelector("#opacityValue")
 const input = document.querySelector("#opacity")
 value.textContent = input.value
-input.addEventListener("input", (event) =>{
+input.addEventListener("input", (event) => {
   value.textContent = event.target.value
-  console.log(input.value)
+  wmsLayer.setOpacity(Number(input.value) * 0.01)
+});
 
-})
-const dateSelector =  document.querySelector("#date")
-
-dateSelector.addEventListener("input", (event) => {
-  console.log(dateSelector.value)
-  currentDate = dateSelector.value
-  url =`https://geoserver2.ymparisto.fi/geoserver/eo/wms?time=${new Date(currentDate).toISOString().substring(0, 10)}`
-  if (reloadAutoCheckBox.checked)  {
-    reloadData()
-    console.log("ADSSSSSSSSSSSSSS")
-  }
-})
 
 
 //let url = "https://geoserver2.ymparisto.fi/geoserver/eo/wms?time=2019-03-22"
 
 //fetch(url).then(function(response) {//
-  //return response.text();
+//return response.text();
 //}).then(function(data) {
-  //console.log(data);
+//console.log(data);
 //}).catch(function(err) {
-  //console.log('Fetch Error :-S', err);
+//console.log('Fetch Error :-S', err);
 //});
 
 //var map = L.map('map').setView([60.192059, 24.945831], 5);
 //L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-  //  maxZoom: 19,
-    //attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+//  maxZoom: 19,
+//attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 //}).addTo(map);
 //var wmsLayer = L.tileLayer.wms(url, {
-  //  layers: 'eo:EO_FSC'
+//  layers: 'eo:EO_FSC'
 //}).addTo(map);
